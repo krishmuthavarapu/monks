cfa73d5f50257cf140a64178729236e9d632c987
sai krishna <krishmuthavarapu@gmail.com>
2019-06-22 18:43:19 +0530

    fc

---
+++ getnow.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Skill Monks</title>
+    <link rel="icon shortcut" href="img/logo.png">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/fbicon.css">
+
+    <style type="text/css">
+        body {}
+
+        /* html,
+    body,
+    header,
+    .view {
+      height: 100%;
+    } */
+
+        @media (max-width: 740px) {
+
+            html,
+            body,
+            header,
+            .view {
+                height: 1300px;
+            }
+        }
+
+        @media (min-width: 700px) and (max-width: 990px) {
+
+            html,
+            body,
+            header,
+            .view {
+                height: 1200px;
+            }
+        }
+
+        @media (min-width: 800px) and (max-width: 850px) {
+
+            html,
+            body,
+            header,
+            .view {
+                height: 1200px;
+            }
+        }
+
+        @media (min-width: 800px) and (max-width: 850px) {
+            .navbar:not(.top-nav-collapse) {
+                background: #1C2331 !important;
+            }
+        }
+        .rgba-black-light-cus, .rgba-black-light-cus:after {
+    background-color: rgba(0, 0, 0, 0.2);
+}
+    </style>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <!-- add navar fixed-top and remove styles and ml-2 -->
+    <nav style="z-index: 100;background: #fff;" class=" navbar-expand-lg navbar-dark ">
+
+    </nav>
+
+    <!-- Navbar -->
+
+    <!-- Full Page Intro -->
+    <div class=" full-page-intro bg-img h-100">
+
+        <!-- Mask & flexbox options-->
+        <div class="mask rgba-black-light-cus d-flex justify-content-center  h-100">
+
+            <div class="container-fluid">
+            <div class="container-fluid">
+        <div class="row ">
+                    <div class="col-6">
+                        <a class="navbar-brand ml-4 pl-lg-4" href="index.php">
+                            <!-- <img src="img/neuronoidsimage.png" width="70px" height="auto" alt=""> -->
+                            <!-- <h3 class="pl-4 pb-2 pt-3">Skill Monks</h3> -->
+                            <img src="img/logo.png" width="100px" alt="">
+                            <!-- <strong>MDB</strong> -->
+                        </a></div>
+                    <div class="col-6 text-center">
+                        <div class="">
+                            <p class="text-dark  font-weight-bolder pt-5">
+                            Email : edu@skillmonks.com &nbsp &nbsp Contact :
+                            9578800900
+
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+        </div>
+                <div class="row wow fadeIn text-center pt-5">
+                    <div class="col pt-5">
+                        <div>
+                            <h1 class="text-dark     font-weight-bold">To Get It Now For Free</h1>
+                            <!-- <h1 class="text-white">Please</h1> -->
+                        </div>
+
+                        <div class="pt-3 pb-3">
+                            <a href="https://www.facebook.com/SKILL-MONKS-1307788232705003/" target="_blank" class="zocial facebook m-3"> Like on Facebook &nbsp;
+                                &nbsp;</a>&nbsp;&nbsp;
+                            <a href="#" class="zocial youtube m-3">Subscribe on YouTube &nbsp;</a>
+                        </div>
+                        <div>
+                            <h5 class="text-white">Or</h5>
+                        </div>
+                        <div>
+                            <h3 class="text-white font-weight-bold pb-3 pt-2">You can also Get Right Now by supporting us</h3>
+                        </div>
+                        <div>
+                                <div class="">
+                                        <div class="">
+                                          <!-- <div class="">
+                                          <input type="radio" name="optradio">	
+                                          </div> -->
+                                        </div>
+                                        <img class="point-cur " src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_150x38.png">
+                                      </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <!-- Mask & flexbox options-->
+
+    </div>
+    <!-- Full Page Intro -->
+
+    <!--Main layout-->
+
+    <!--Main layout-->
+    <!-- Projects section v.3 -->
+    <!-- Section: Features v.1 -->
+
+    <!-- Section: Features v.1 -->
+    <!-- Projects section v.3 -->
+    <!--Footer-->
+
+    <footer class="page-footer text-center font-small wow fadeIn">
+
+        <!-- Social icons -->
+        <!-- <div class="pb-4">
+      <a href="https://www.facebook.com/mdbootstrap" target="_blank">
+        <i class="fab fa-facebook-f mr-3"></i>
+      </a>
+
+      <a href="https://twitter.com/MDBootstrap" target="_blank">
+        <i class="fab fa-twitter mr-3"></i>
+      </a>
+
+      <a href="https://www.youtube.com/watch?v=7MUISDJ5ZZ4" target="_blank">
+        <i class="fab fa-youtube mr-3"></i>
+      </a>
+
+      <a href="https://plus.google.com/u/0/b/107863090883699620484" target="_blank">
+        <i class="fab fa-google-plus-g mr-3"></i>
+      </a>
+
+      <a href="https://dribbble.com/mdbootstrap" target="_blank">
+        <i class="fab fa-dribbble mr-3"></i>
+      </a>
+
+      <a href="https://pinterest.com/mdbootstrap" target="_blank">
+        <i class="fab fa-pinterest mr-3"></i>
+      </a>
+
+      <a href="https://github.com/mdbootstrap/bootstrap-material-design" target="_blank">
+        <i class="fab fa-github mr-3"></i>
+      </a>
+
+      <a href="http://codepen.io/mdbootstrap/" target="_blank">
+        <i class="fab fa-codepen mr-3"></i>
+      </a>
+    </div> -->
+        <!-- Social icons -->
+
+        <!--Copyright-->
+        <div class="footer-copyright py-3">
+            © 2019 Copyright:
+            <a href="www.skillmonks.in" target="_blank">skillmonks.com </a>
+            All Rights Reserved
+        </div>
+        <!--/.Copyright-->
+
+    </footer>
+    <script>
+
+        $(function () {
+            $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+        });</script>
+    <!--/.Footer-->
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Initializations -->
+    <script type="text/javascript">
+    // Animations initialization
+    // new WOW().init();
+    </script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Skill Monks</title>
+    <link rel="icon shortcut" href="img/logo.png">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- fonts -->
+<link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Open+Sans&display=swap" rel="stylesheet">    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <style type="text/css">
+        body {}
+
+        /* html,
+    body,
+    header,
+    .view {
+      height: 100%;
+    } */
+
+        @media (max-width: 740px) {
+
+            html,
+            body,
+            header,
+            .view {
+                height: 1300px;
+            }
+        }
+
+        @media (min-width: 700px) and (max-width: 990px) {
+
+            html,
+            body,
+            header,
+            .view {
+                height: 1200px;
+            }
+        }
+
+        @media (min-width: 800px) and (max-width: 850px) {
+
+            html,
+            body,
+            header,
+            .view {
+                height: 1200px;
+            }
+        }
+
+        @media (min-width: 800px) and (max-width: 850px) {
+            .navbar:not(.top-nav-collapse) {
+                background: #1C2331 !important;
+            }
+        }
+    </style>
+</head>
+
+<body>
+
+
+
+    <!-- Navbar -->
+
+    <!-- Full Page Intro -->
+    <div class=" full-page-intro bg-img ">
+
+        <!-- Mask & flexbox options-->
+        <div class="mask d-flex justify-content-center align-items-center ">
+
+
+            <!-- Content -->
+            <div class="container pb-100 mb-4">
+                <div class="row ">
+                    <div class="col-6">
+                        <a class="navbar-brand " href="index.php">
+                            <!-- <img src="img/neuronoidsimage.png" width="70px" height="auto" alt=""> -->
+                            <!-- <h3 class="pl-4 pb-2 pt-3">Skill Monks</h3> -->
+                            <img src="img/logo.png" width="130px" alt="">
+                            <!-- <strong>MDB</strong> -->
+                        </a></div>
+                    <div class="col-6 text-center">
+                        <div class="">
+                            <p class="text-dark  font-weight-bolder pt-2 pt-lg-5">
+                                Email : edu@skillmonks.com &nbsp &nbsp Contact :
+                                9578800900
+
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <!--Grid row-->
+                <div class="row pt-3 mt-3 pb-3">
+                    <div class="col text-center ">
+                        <h2 class="" style="font-weight:400"><span class="p-1">Welcome to</span><span class="p-1" style="color: #01783d;">SkillMonks</span></h2>
+                        <!-- <h2 class="h2-responsive weight-400"><span class="skbc p-1">Welcome </span><span style="" class="p-1 bg-dark text-white"> to SkillMonks</span></h2> -->
+                    </div>
+                </div>
+                <div class="row wow fadeIn pb-100 mb-5 pt-3 mt-3">
+                    <div class="col text-center align-self-center">
+                        <div class="overlay zoom">
+                            <button type="button" onclick="location.href='selection.php'" class="btn btn-default btn skbc btn-rounded "><div class="h5-responsive">Student</div></button>
+                        </div>
+                    </div>
+                    <div class="col text-center align-self-center ">
+                    <div class="overlay zoom">
+                            <button type="button" class="btn btn-default btn skbc btn-rounded"><div class="h5-responsive">Institute</div></button>
+                        </div>
+                    </div>
+                    <div class="col text-center align-self-center">
+                    <div class="overlay zoom">
+                            <button type="button" class="btn btn-default btn skbc btn-rounded"><div class="h5-responsive">Admin</div></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- Mask & flexbox options-->
+
+    </div>
+    <!-- Full Page Intro -->
+    <!--Footer-->
+
+    <footer class="page-footer text-center font-small wow fadeIn">
+
+        <!-- Social icons -->
+        <!-- <div class="pb-4">
+      <a href="https://www.facebook.com/mdbootstrap" target="_blank">
+        <i class="fab fa-facebook-f mr-3"></i>
+      </a>
+
+      <a href="https://twitter.com/MDBootstrap" target="_blank">
+        <i class="fab fa-twitter mr-3"></i>
+      </a>
+
+      <a href="https://www.youtube.com/watch?v=7MUISDJ5ZZ4" target="_blank">
+        <i class="fab fa-youtube mr-3"></i>
+      </a>
+
+      <a href="https://plus.google.com/u/0/b/107863090883699620484" target="_blank">
+        <i class="fab fa-google-plus-g mr-3"></i>
+      </a>
+
+      <a href="https://dribbble.com/mdbootstrap" target="_blank">
+        <i class="fab fa-dribbble mr-3"></i>
+      </a>
+
+      <a href="https://pinterest.com/mdbootstrap" target="_blank">
+        <i class="fab fa-pinterest mr-3"></i>
+      </a>
+
+      <a href="https://github.com/mdbootstrap/bootstrap-material-design" target="_blank">
+        <i class="fab fa-github mr-3"></i>
+      </a>
+
+      <a href="http://codepen.io/mdbootstrap/" target="_blank">
+        <i class="fab fa-codepen mr-3"></i>
+      </a>
+    </div> -->
+        <!-- Social icons -->
+
+        <!--Copyright-->
+        <div class="footer-copyright py-3">
+            © 2019 Copyright:
+            <a href="www.skillmonks.in" target="_blank">skillmonks.com </a>
+            All Rights Reserved
+        </div>
+        <!--/.Copyright-->
+
+    </footer>
+    <script>
+        $(function() {
+            $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+        });
+    </script>
+    <!--/.Footer-->
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Initializations -->
+    <script type="text/javascript">
+        // Animations initialization
+        // new WOW().init();
+    </script>
+</body>
+
+</html>
